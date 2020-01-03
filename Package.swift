c36ddc52b6edb6a6c8daf286a0eae1337cc708e7
Mats Eikeland Mollestad <mem@mollestad.no>
2020-01-03 14:56:48 +0100

    [Package][Renaming] Updated HTMLKit version and renamed the package

--- Package.swift
+++ Package.swift
@@ -4,30 +4,33 @@
 import PackageDescription
 
 let package = Package(
-    name: "HTMLKit-Vapor-3-Provider",
+    name: "HTMLKitVaporProvider",
+    platforms: [
+      .macOS(.v10_14),
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "HTMLKit-Vapor-3-Provider",
-            targets: ["HTMLKit-Vapor-3-Provider"]),
+            name: "HTMLKitVaporProvider",
+            targets: ["HTMLKitVaporProvider"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/vapor-community/HTMLKit.git", from: "2.0.0-beta.2"),
+        .package(url: "https://github.com/vapor-community/HTMLKit.git", from: "2.0.0-beta.3"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "HTMLKit-Vapor-3-Provider",
+            name: "HTMLKitVaporProvider",
             dependencies: [
                 "HTMLKit",
                 "Vapor"
         ]),
         .testTarget(
-            name: "HTMLKit-Vapor-3-ProviderTests",
-            dependencies: ["HTMLKit-Vapor-3-Provider"]),
+            name: "HTMLKitVaporProviderTests",
+            dependencies: ["HTMLKitVaporProvider"]),
     ]
 )
